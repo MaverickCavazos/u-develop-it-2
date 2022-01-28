22955723f13f31dfa6b0b8e9a0a0a99abc0a91a0
Maverick <Mav33987@gmail.com>
2022-01-27 18:15:45 -0600

    connected sql db to server

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
-app.use(express.urlencoded({ extended: false  }));
+//middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: '1nnd90MR$!',
+        database: 'election'
+    },
+    console.log('Connected to the elction database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 app.use((req, res) => {
     res.status(404).end();

88c03e29f5a19e2e4f9cc162e1b59cf490a1186f
Maverick <Mav33987@gmail.com>
2022-01-27 18:26:18 -0600

    added db queries

--- server.js
+++ server.js
@@ -20,10 +20,36 @@ const db = mysql.createConnection(
     console.log('Connected to the elction database')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
+/* const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?, ?, ?, ?)`;
+                const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if(err) {
+        console.log(err)
+    }
+    console.log(result);
+}); */
+
+/* db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) =>{
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+}); */
+
+/* db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+ */
+
+/* db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
   });
-
+ */
 app.use((req, res) => {
     res.status(404).end();
 });
